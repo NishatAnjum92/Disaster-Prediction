6830059f608ddf62b221b96c91af3f42446df065
NishatAnjum92 <41347265+NishatAnjum92@users.noreply.github.com>
2018-07-18 22:21:45 +0600

    Add files via upload

---
+++ decisiontree.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+import matplotlib
+import matplotlib.pyplot as plt
+from IPython import get_ipython
+import pydotplus
+from titanic_visualizations import survival_stats
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+def gini_index(groups, classes):
+    n_instances = float(sum([len(group) for group in groups]))
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        if size == 0:
+            continue
+        score = 0.0
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+print(gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
+print(gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
+
+
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    return left, right
+
+
+def get_split(dataset):
+    class_values = list(set(row[-1] for row in dataset))
+    b_index, b_value, b_score, b_groups = 999, 999, 999, None
+    for index in range(len(dataset[0])-1):
+        for row in dataset:
+            groups = test_split(index, row[index], dataset)
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index': b_index, 'value': b_value, 'groups': b_groups}
